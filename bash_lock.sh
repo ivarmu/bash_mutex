75efb79a63472919b36c3385ff9b3731293f964f
Ivan Aragonés Muniesa <iaragone@redhat.com>
2019-01-02 16:22:50 +0100

    I differentiate the mutex for an specified command and lock/unlock for more complex behaviour

--- bash_lock.sh
+++ bash_lock.sh
@@ -3,25 +3,33 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # 
-# Usage: bash_mutex.sh <-m 'mutex_name'> [-s] [-r] [-p] [-n <max_queue_length>] [-ml <max_lock_time>] [-mw <max_wait_time>] "command to run in a semaphore-like manner"
+# Usage: bash_lock.sh <-m 'mutex_name'> [-s] [-r] [-p] [-n <max_queue_length>] [-ml <max_lock_time>] [-mw <max_wait_time>]
 #
-# Example: bash_mutex.sh -m 'echo' 'echo $$ >> /tmp/test'"
-# Example: bash_mutex.sh -m 'echo2' -n 3 'echo $$ >> /tmp/test'"
-# Example: bash_mutex.sh -m 'mutex' -ml 60 'echo $$ >> /tmp/test'"
-# Example: bash_mutex.sh -m 'mutex2' -mw 30 'echo $$ >> /tmp/test'"
-# Example: bash_mutex.sh -m 'label1' -n 3 -ml 60 -mw 30 'echo $$ >> /tmp/test'"
+# Example: bash_lock.sh -m 'get_static_mutex' -s
+# Example: bash_lock.sh -m 'get_static_mutex' -s -mw 30
+# Example: bash_lock.sh -m 'program_end_of_static_mutex' -p -ml 30 &
+# Example: bash_lock.sh -m 'release_static_mutex' -r
+#
+# NOTE: '-s', '-p' and '-r' options are used to set and release the mutexes manually for long running or multi-command mutex requirements.
+# In that cases, the calling process is the responsible of the correct muttex release as <max_lock_time> is completely ignored.
+#
+# NOTE: '-p' may be usually executed in background (&), as it only programs the lock auto-release (don't execute any command).
 #
 
 # Check for the input parameters
 if [ $# -eq 0 ]; then
   echo ""
-  echo "Usage: bash_mutex.sh <-m 'mutex_name'> [-n <max_queue_length>] [-ml <max_lock_time>] [-mw <max_wait_time>] \"command to run in a semaphore-like manner\""
+  echo "Usage: bash_lock.sh <-m 'mutex_name'> [-s] [-r] [-p] [-n <max_queue_length>] [-ml <max_lock_time>] [-mw <max_wait_time>]"
   echo ""
-  echo "Example: bash_mutex.sh -m 'echo' 'echo $$ >> /tmp/test'"
-  echo "Example: bash_mutex.sh -m 'echo2' -n 3 'echo $$ >> /tmp/test'"
-  echo "Example: bash_mutex.sh -m 'mutex' -ml 60 'echo $$ >> /tmp/test'"
-  echo "Example: bash_mutex.sh -m 'mutex2' -mw 30 'echo $$ >> /tmp/test'"
-  echo "Example: bash_mutex.sh -m 'label1' -n 3 -ml 60 -mw 30 'echo $$ >> /tmp/test'"
+  echo "Example: bash_lock.sh -m 'get_static_mutex' -s"
+  echo "Example: bash_lock.sh -m 'get_static_mutex' -s -n 3 -mw 30"
+  echo "Example: bash_lock.sh -m 'program_end_of_static_mutex' -p -ml 30"
+  echo "Example: bash_lock.sh -m 'release_static_mutex' -r"
+  echo ""
+  echo "NOTE: '-s', '-p' and '-r' options are used to set and release the mutexes manually for long running or multi-command mutex requirements."
+  echo "In '-s' and '-r' cases, the calling process is the responsible of the correct mutex release as <max_lock_time> is completely ignored."
+  echo ""
+  echo "NOTE: '-p' may be usually executed in background (&), as it only programs the lock auto-release as per <max_lock_time> specified."
   echo ""
   echo "Error codes:"
   echo "  0: no errors"
@@ -41,7 +49,7 @@ fi
 
 # Lock directory
 _LOCK_PATH="${LOCK_PATH:-/var/tmp}"
-_MUTEX_PREFIX="bash_mutex_"
+_MUTEX_PREFIX="bash_lock_"
 _MUTEX_NAME=""
 
 # Maximum amount of time to maintain the lock (in seconds)
@@ -50,6 +58,9 @@ _MAX_LOCK_TIME=5
 _MAX_WAIT_TIME=60
 # By default, no limit on number of processes waiting for the mutex.
 let _MAX_QUEUE_LEN=0
+_ONLY_GET_LOCK=0
+_PROGRAM_RELEASE=0
+_RUN_RELEASE=0
 
 # Parse arguments
 if [ "${1}" == "-m" ]; then
@@ -78,11 +89,26 @@ while [ "${1#-}" != "${1}" ]; do
           shift
           shift
           ;;
+    "-s")
+          _ONLY_GET_LOCK=1
+          shift
+          ;;
+    "-p")
+          _PROGRAM_RELEASE=1
+          shift
+          ;;
+    "-r")
+          _RUN_RELEASE=1
+          shift
+          ;;
   esac
 done
 
 _LOCK_DIR="${_LOCK_PATH}/${_MUTEX_PREFIX}${_MUTEX_NAME}.lck"
 
+echo "_ONLY_GET_LOCK = ${_ONLY_GET_LOCK}"
+echo "_PROGRAM_RELEASE = ${_PROGRAM_RELEASE}"
+echo "_RUN_RELEASE = ${_RUN_RELEASE}"
 echo "_MAX_LOCK_TIME = ${_MAX_LOCK_TIME}"
 echo "_MAX_WAIT_TIME = ${_MAX_WAIT_TIME}"
 echo "_MAX_QUEUE_LEN = ${_MAX_QUEUE_LEN}"
@@ -94,7 +120,7 @@ let _SUM_LOCK_TIME=_MAX_LOCK_TIME+_MAX_WAIT_TIME
 
 function clean_exit {
   # remove us from the queue
-  queue_out
+  rmdir ${_LOCK_DIR}_$$ &>/dev/null
 
   # if we got the lock, release it
   if [ -f ${_LOCK_DIR}/info.txt ]; then
@@ -106,18 +132,18 @@ function clean_exit {
   exit ${1:-0}
 }
 
-# Function to get into the queue (controlled by mutex)
+# Function to get into the queue
 function queue_in {
   # if the queue is full, exit before try getting the lock
   if [ ${_MAX_QUEUE_LEN} -ne 0 ]; then
-    # put ourselves in the mutex queue to write to the queue (taking care of _MAX_QUEUE_LEN)
-    ${0} -m 'queue' -n 0 -mw 3 -ml 3 " \
+    # put ourselves in the mutex queue to write to the queue
+    ${0%/*}/bash_mutex.sh -m 'queue' -n 0 -mw 3 -ml 3 " \
       if [ \$(ls -d ${_LOCK_DIR}_* 2>/dev/null | wc -l) -lt ${_MAX_QUEUE_LEN} ]; then \
         mkdir ${_LOCK_DIR}_$$ >&/dev/null; \
       else \
         exit 104; \
       fi; \
-    " || clean_exit 104
+    " &>/dev/null || clean_exit 104
     if [ $? -ne 0 ]; then
       echo "Max queue length has been reached. No command is executed from $$"
       clean_exit 104
@@ -125,7 +151,6 @@ function queue_in {
   fi
 }
 
-# Function to get out from the queue
 function queue_out {
   rmdir ${_LOCK_DIR}_$$ &>/dev/null
 }
@@ -133,10 +158,9 @@ function queue_out {
 # Function to lock
 function lock {
   let _counter=1
-  # wait to get the lock
   while ! mkdir ${_LOCK_DIR} &>/dev/null; do
     if [ ${_counter} -gt ${_MAX_WAIT_TIME} ]; then
-      echo "Could'nt get the lock: timed out... No command is executed from $$"
+      echo "Could'nt get the lock: timed out..."
       clean_exit 105
       break
     fi
@@ -150,6 +174,8 @@ function lock {
 
 # Function to unlock 
 function unlock {
+  # first of all, get out of the queue to let another process to get the slot
+  rmdir ${_LOCK_DIR}_$$ &>/dev/null
   # Can remove the auto-unlock timer and all it's childs
   if [ ! -z "${_ALARM_GENERATOR_PID}" ]; then
     _procs="$(pstree -p ${_ALARM_GENERATOR_PID} | grep -Po '[^[:digit:]]*\K[[:digit:]]*' | sort -nr)"
@@ -168,23 +194,18 @@ function unlock {
       done
     fi
   fi
-  # free the lock
+  # finally, release the lock
   rm -rf ${_LOCK_DIR} &>/dev/null
 }
 
 # Configure SIGALRM handler
 function unlock_signal {
   echo "handling the received signal: ${1}"
-  # first of all, get out of the queue to let another process to get the slot
-  queue_out
   unlock ${1}
   exit 103
 }
 
 # MAIN
-
-# Put in the queue
-queue_in
 
 trap 'unlock_signal ALRM' ALRM
 trap 'clean_exit 2' INT
@@ -192,22 +213,25 @@ trap 'clean_exit 15' TERM
 trap 'clean_exit 1' HUP
 trap 'clean_exit 6' ABRT
 
-(sleep ${_SUM_LOCK_TIME}; kill -SIGALRM $$ &>/dev/null; exit 0)&
-_ALARM_GENERATOR_PID=$!
+if [ ${_RUN_RELEASE} -eq 1 ]; then
+  # If called with -r we only need to release the lock
+  rm -rf ${_LOCK_DIR} &> /dev/null
+  clean_exit 0
+elif [ ${_PROGRAM_RELEASE} -eq 1 ]; then
+  # If called with -p we program auto-unlock
+  (sleep ${_SUM_LOCK_TIME}; kill -SIGALRM $$ &>/dev/null; exit 0)&
+  _ALARM_GENERATOR_PID=$!
+  echo "$(hostname) - PID: $$" > ${_LOCK_DIR}/info.txt && sync
+  wait ${_ALARM_GENERATOR_PID}
+  # Exit assuring the lock has been released
+  unlock 0 
+  exit 0
+elif [ ${_ONLY_GET_LOCK} ]; then
+  # If called with -s we try to get the lock
+  queue_in
+  lock
+fi
 
-# Get the lock
-lock
-
-# MAIN process
-echo "$(date) - executing the given command: $@"
-eval $@ &
-_COMMAND=$!
-wait ${_COMMAND}
-result=$?
-
-# Release the lock and exit
-clean_exit ${result}
-
-# Never reached
-exit ${result}
+# Clean exit
+exit 0
 
